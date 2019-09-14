6f74f716b474d0966a2e842c6f5c53645aa2f65b
markmumba <markmumba01@gmail.com>
2019-09-14 15:50:02 +0300

    finished with the registration forms

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profile/', null=True)
+    user = models.TextField(null=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    email = models.EmailField(null=True)
+
+    def __str__(self):
+        return self.user
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()

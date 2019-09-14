7c7d4f27576030593bfe4c6904b63698e6c5df58
markmumba <markmumba01@gmail.com>
2019-09-14 15:14:44 +0300

    added the registration forms

--- HOOD/settings.py
+++ HOOD/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'neighbourhood',
-    'bootstrap3'
+    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

bd595bf613d557cd2bd0d52bfd2f30ad32938140
markmumba <markmumba01@gmail.com>
2019-09-15 14:24:31 +0300

    added the models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -17,3 +17,37 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=40)
+    location = models.ForeignKey(Location, on_delete==models.CASCADE)
+    occupants=models.IntegerField(null=True, default=0)
+    
+    def __str__(self):
+        return self.name
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls,neigborhood_id):
+        neighborhood = cls.objects.get(id = neigborhood_id)
+        return neighborhood
+
+    def update_neighborhood(self):
+        self.save()
+
+    def update_occupants(self):
+        self.occupants += 1
+        self.save()

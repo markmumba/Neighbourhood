f879b4f74c4bbcb83d83ab05b3fb2c3fb8ab4ba8
markmumba <markmumba01@gmail.com>
2019-09-15 17:39:19 +0300

    the business models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -48,3 +48,16 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Business(models.Model):
+    picture=models.ImageField(upload_to='picture/')
+    description=HTMLField()
+    neighbourhood=models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    owner=models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address =models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+        
